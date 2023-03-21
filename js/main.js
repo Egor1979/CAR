5ad553f795f4bbc60ac997ce5557a2d1b1ee5d28
Georgy <kegora9@gmail.com>
2023-03-21 14:08:23 +0300

    сделал секшин

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $(function(){
+$('.top__slider').slick({//обращаюсь к родител
+    arrows:false,//не будет стрелок смотри в слике внизу в докумет
+    dots:true,//появю кнопки
+    autoplay:true,
+    fade:true,//автопрокрутка
+});
 
 });
